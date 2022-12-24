fe67ad2916317bf65ba33bbe3d9007588beb91ae
Na Mi <m.na@60hz.io>
2022-12-24 10:38:42 +0900

    cors 설치

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  function getHello() {
+    const greet = document.getElementById('greet')
+    fetch('http://localhost:4000/api/hello')
+      .then(response => response.json())
+      .then(data => greet.innerHTML = JSON.stringify(data))
+  }
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    getHello();
+  }, [count])
 
   return (
     <div className="App">
       <h3>테스트</h3>
       <p>야호! 배포 성공!</p>
       <p>CI/CD도 성공했어!</p>
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        api called: <code id="greet"></code>
+      </p>
     </div>
   )
 }
